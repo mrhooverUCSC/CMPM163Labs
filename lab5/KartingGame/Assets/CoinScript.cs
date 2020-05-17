a56cff6c186387c671fe2f3e4cd4245694b2e672
Matthew Hoover <mrhoover@ucsc.edu>
2020-05-16 17:49:16 -0700

    lab5/6

---
+++ lab5/KartingGame/Assets/CoinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Update is called once per frame
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
